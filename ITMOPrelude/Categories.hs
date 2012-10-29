016b617e59607c3af63b39c9948e8e2a8f19e62e
Anastasia Lebedeva <lebedeva.anestezia@gmail.com>
2012-10-30 01:20:39 +0300

    hw4-4

--- ITMOPrelude/Categories.hs
+++ ITMOPrelude/Categories.hs
@@ -1,25 +1,35 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module ITMOPrelude.Categories where
 
+-- Реализовать для всего,
+-- что только можно из
+import ITMOPrelude.Primitive
 import ITMOPrelude.List
 import ITMOPrelude.Tree
-import Prelude(error)
+-- всевозможные инстансы для классов ниже
 
+--------------------------------------------------------------------------------
+-- Классы
+class Category cat where
+    id  :: cat a a
+    (.) :: cat b c -> cat a b -> cat a c
 
-class Category f where
---	id :: f a a
-	(.) :: f b c -> f a b -> f a c
-                       
 class Functor f where
-	fmap :: (a -> b) -> f a -> f b
-
+    fmap :: (a -> b) -> f a -> f b
 
 class Monad m where
-	return :: a -> m a	
-	(>>=) :: m a -> (a -> m b) -> m b
-	(>>) :: m a -> m b -> m b
-	x >> y = x >>= \_ -> y 
+    return :: a -> m a
+    (>>=) :: m a -> (a -> m b) -> m b
 
+(>>) :: Monad m => m a -> m b -> m b
+ma >> mb = ma >>= (\_ -> mb)
+
+--------------------------------------------------------------------------------
+-- Инстансы писать сюда
+
+instance Category (->) where
+	id = \x -> x
+	f . g = \x -> f (g x)
 
 instance Functor List where
 	fmap = map
@@ -31,3 +41,15 @@ instance Functor Tree where
 instance Monad List where
 	return a = Cons a Nil
 	a >>= f = concatMap f a
+
+
+--------------------------------------------------------------------------------
+-- Монада State
+
+newtype State s a = State { runState :: s -> (s, a) }
+
+instance Monad (State s) where
+    return x = State (\s -> (x, s))
+    (State h) >>= f = State $ \s -> let (newstate, a) = h s
+				(State g) = f a
+			in g newstate	
